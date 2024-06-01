6030ce704cb195e8d37979529a62b66e164bf8b5
CzzzzzzJ <427999001@qq.com>
2024-06-01 18:41:38 +0800

    fix_name

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ import { seo } from '~/lib/seo'
 export const metadata: Metadata = {
   metadataBase: seo.url,
   title: {
-    template: '%s | Cali Castle',
+    template: '%s | CoXcommunity',
     default: seo.title,
   },
   description: seo.description,
-  keywords: 'Cali,Cali Castle,郭晓楠,佐玩,创始人,CEO,开发者,设计师,细节控,创新',
+  keywords: 'Cali,CoXcommunity,郭晓楠,佐玩,创始人,CEO,开发者,设计师,细节控,创新',
   manifest: '/site.webmanifest',
   robots: {
     index: true,
@@ -34,10 +34,10 @@ export const metadata: Metadata = {
   openGraph: {
     title: {
       default: seo.title,
-      template: '%s | Cali Castle',
+      template: '%s | CoXcommunity',
     },
     description: seo.description,
-    siteName: 'Cali Castle',
+    siteName: 'CoXcommunity',
     locale: 'zh_CN',
     type: 'website',
     url: 'https://cali.so',
